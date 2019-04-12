fbf2eac4e6e8d16579892024fc18c55242c84365
visualstudio <visualstudio@122-PC40>
2019-04-12 19:49:02 -0400

    program2

--- program 2/Program.cs
+++ program 2/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace program_2
+namespace TextAnalysisTools
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the longest word is :");
+
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
         }
-    }
-}
